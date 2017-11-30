c04b59bbba101700aa0bd890d9e0236440318e7e
sasidharkm <sasidhar.km97@gmail.com>
2017-11-30 09:20:48 +0530

    Created the Vector Class

--- linalg.py
+++ linalg.py
@@ -1,1 +1,16 @@
 #Just a library to code linear algebraic functions
+
+class Vector(object):
+	def __init__(self, coordinates):
+		try:
+			if not coordinates:
+				raise ValueError
+			self.coordinates = tuple(coordinates)
+			self.dimension = len(coordinates)
+
+		except ValueError:
+			raise ValueError('The coordinates must be non-empty')
+
+		except TypeError:
+			raise TypeError('The coordinates must be an iterable')
+			
